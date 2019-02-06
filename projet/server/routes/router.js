38f617e9f0f2e52be042043ac9dfccd4a1f2e161
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-06 09:49:42 +0100

    Create working api

--- projet/server/routes/router.js
+++ projet/server/routes/router.js
@@ -3,8 +3,8 @@ const LumiereController = require('../controllers/lumiere_controller');
 
 const router = express.Router();
 
-router.post('/lumiere', LumiereController.latest);
-router.get('/lumiere', LumiereController.latest);
+router.post('/lumiere', LumiereController.put);
+router.get('/lumiere', LumiereController.get);
 router.get('/lumiere/latest', LumiereController.latest);
 
 module.exports =  router;
