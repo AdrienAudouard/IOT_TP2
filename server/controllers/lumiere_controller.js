6fe86b1e3737b7e01a0c9303e73ead5b26d42a05
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-11 22:11:32 +0100

    Add count query to lumiere api

--- server/controllers/lumiere_controller.js
+++ server/controllers/lumiere_controller.js
@@ -2,11 +2,26 @@ const Lumiere = require('../models/lumiere');
 
 class LumiereController {
   get(req, res) {
-    Lumiere.find({}).then((lums) => {
-      res.status(200).send({success: true, result: lums});
-    }).catch((err) => {
-      res.status(400).send({succes: false, err});
-    })
+    let count = parseInt(req.query.count) || 20;
+
+    if (count === NaN) {
+      res.status(400).send({success: false, message: 'Count must be an integer'});
+      return;
+    }
+
+    if (count < 1) {
+      res.status(400).send({success: false, message: 'Count must be greater than 0'});
+      return;
+    }
+
+    Lumiere.find({}).sort({'date' : -1}).limit(count).exec((err, result) => {
+      if (err !== null) {
+        res.status(400).send({success: false, err});
+        return;
+      }
+
+      res.status(200).send({success: true, result: result, count});
+    });
   }
 
   put(req, res) {
@@ -17,7 +32,7 @@ class LumiereController {
     Lumiere.create({lumiere : value.lumiere}).then((lum) => {
       res.status(200).send({success: true, result: lum});
     }).catch((err) => {
-      res.status(400).send({succes: false, err});
+      res.status(400).send({success: false, err});
     });
   }
 
@@ -25,7 +40,7 @@ class LumiereController {
     Lumiere.findOne({}, {}, { sort: {'date' : -1} }).then((lum) => {
       res.status(200).send({success: true, result: lum});
     }).catch((err) => {
-      res.status(400).send({succes: false, err});
+      res.status(400).send({success: false, err});
     });
   }
 }
