4a2ad88bb081f457b33180b688bb4744a43184dd
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-12 11:18:51 +0100

    Add secure router

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,10 +4,6 @@ const LedStateController = require('../controllers/led_state_controller');
 const router = express.Router();
 
 router.post('/lumiere', LumiereController.put);
-router.get('/lumiere', LumiereController.get);
-router.get('/lumiere/latest', LumiereController.latest);
-
-router.get('/led_state', LedStateController.get);
 router.post('/led_state', LedStateController.put);
 router.put('/led_state', LedStateController.put);
 
