65acc74f885f76b191f7aa4d94397d05bdb2fb0c
Adrien Audouard <adrien.audouard@gmail.com>
2019-02-13 08:44:30 +0100

    Fix broker

--- server/broker/broker.js
+++ server/broker/broker.js
@@ -2,14 +2,12 @@ const mqtt = require('mqtt');
 const url = require('url');
 // Parse
 const mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-const auth = (mqtt_url.auth || ':').split(':');
 
 class Broker {
     start() {
         this.client = mqtt.connect(mqtt_url);
         this.client.on('connect', () => {
             this.client.subscribe('myTopic');
-            console.log('mqtt connected to: ' + mqtt_url);
         });
 
         this.client.on('message', function (topic, message) {
